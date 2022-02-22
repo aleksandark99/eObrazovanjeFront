d73f601a724a6a50c26d094142d22aa428d70a89
Aleksandar Kotur <aleksa.kotur@gmail.com>
2022-02-22 04:22:32 +0100

    small fix

--- src/app/components/register-for-test/register-for-test.component.ts
+++ src/app/components/register-for-test/register-for-test.component.ts
@@ -19,8 +19,8 @@ export class RegisterForTestComponent implements OnInit {
     this.examPeriodService.getActiveExamPeriodsUsingGET("body").subscribe((res) => (this.optionsExamPeriod = res));
   }
 
-  singleSelect: any = [];
-  singleSelect2: any = [];
+  singleSelect: any = null;
+  singleSelect2: any = null;
   config = {
     displayKey: "name", // if objects array passed which key to be displayed defaults to description
   };
@@ -31,10 +31,32 @@ export class RegisterForTestComponent implements OnInit {
   selectionChanged($event) {
     // console.log($event.value.id);
     if ($event.value.id != null) {
+      console.log("aasd")
       this.examPeriodService.getTestsForExamPeriodAndStudentUsingGET($event.value.id, "body").subscribe((res) => (this.optionsTest = res));
-      this.examPeriodService.getListOfMyTestsUsingGET($event.value.id, "body").subscribe((res) => (this.idsOfStudentTests = res));
+      this.examPeriodService.getListOfMyTestsUsingGET($event.value.id, "body").subscribe((res) => {
+        this.idsOfStudentTests = res;
+        this.addTestsIntoDropDown();
+
+      });
     }else{
-      this.optionsTest=[];
+      this.optionsTest=null;
+      this.singleSelect2=null;
+    }
+  }
+
+  // Adds tests that user didnt already register for 
+  addTestsIntoDropDown(){
+    this.optionsTest = this.optionsTest.filter(test => {
+      return !this.idsOfStudentTests.includes(test.id);
+    })
+  }
+
+  registerForTest(){
+    if(this.singleSelect2.id!=null){
+      alert(this.singleSelect2.id)
+      console.log(this.singleSelect2)
+    }else{
+      alert('You need to select a test you want to register for')
     }
   }
 }
