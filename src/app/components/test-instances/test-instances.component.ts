87e3eb781de3517a2eaa7f69ca57fd75e45bc1b5
Aleksandar Kotur <aleksa.kotur@gmail.com>
2022-02-23 08:28:42 +0100

    test instances

--- src/app/components/test-instances/test-instances.component.ts
+++ src/app/components/test-instances/test-instances.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { TestControllerService, TestInstanceWithUser } from "src/app/api";
 
 @Component({
-  selector: 'app-test-instances',
-  templateUrl: './test-instances.component.html',
-  styleUrls: ['./test-instances.component.scss']
+  selector: "app-test-instances",
+  templateUrl: "./test-instances.component.html",
+  styleUrls: ["./test-instances.component.scss"],
 })
 export class TestInstancesComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  testId = null;
+  courseInstances: TestInstanceWithUser[] = [];
+  constructor(private route: ActivatedRoute, private testInstanceService: TestControllerService) {
+    this.route.queryParams.subscribe((params) => {
+      this.testId = params.testId;
+    });
   }
 
+  ngOnInit(): void {
+    this.testInstanceService
+      .getTestInstancesUsingGET(this.testId, "body")
+      .toPromise()
+      .then((res) => (this.courseInstances = res))
+      .catch((err) => console.warn(err));
+  }
 }
