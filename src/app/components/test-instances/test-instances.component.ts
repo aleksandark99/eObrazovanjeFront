4e887bbc68b1d6f658f21fcc55ef460133f388d1
Aleksandar Kotur <aleksa.kotur@gmail.com>
2022-02-23 09:07:28 +0100

    add grading tests

--- src/app/components/test-instances/test-instances.component.ts
+++ src/app/components/test-instances/test-instances.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { TestControllerService, TestInstanceWithUser } from "src/app/api";
-
+import { FormGroup } from "@angular/forms";
+import { FormlyFormOptions, FormlyFieldConfig } from "@ngx-formly/core";
 @Component({
   selector: "app-test-instances",
   templateUrl: "./test-instances.component.html",
@@ -23,4 +24,52 @@ export class TestInstancesComponent implements OnInit {
       .then((res) => (this.courseInstances = res))
       .catch((err) => console.warn(err));
   }
+
+  gradeTest(model){
+    if(this.form.valid){
+      console.log(model)
+    }else{
+      alert("Incomplete form")
+    }
+  }
+
+  form = new FormGroup({});
+
+  model: any = {};
+  options: FormlyFormOptions = {};
+  fields: FormlyFieldConfig[] = [
+    {
+      className: "section-label",
+      template: "<hr /><div><strong>Create new course:</strong></div>",
+    },
+    {
+      fieldGroupClassName: "row",
+      fieldGroup: [
+        {
+          className: "col-3",
+          type: "input",
+          key: "testInstanceId",
+          templateOptions: {
+            type: "number",
+            label: "Id",
+            max: 30,
+            min: 0,
+            required: true,
+          },
+        },
+        {
+          className: "col-3",
+          type: "input",
+          key: "points",
+          templateOptions: {
+            type: "number",
+            label: "Points",
+            max: 30,
+            min: 0,
+            required: true,
+          },
+        },
+      ],
+    },
+  ];
 }
